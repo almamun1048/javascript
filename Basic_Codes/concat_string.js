34d7a8576a09ea8f811c03d5ff1206fdc62fa001
almamun1048 <programmeralmamun@gmail.com>
2022-05-31 23:24:44 +0600

    string concatenate

---
+++ Basic_Codes/concat_string.js
@@ -0,0 +1,26 @@
+
+//Concatenate String
+
+console.log("Al "+"Mamun");
+
+var fname = "Al ";
+var lname = "Mamun";
+var fullName = fname + lname;
+console.log(fullName);
+
+console.log("My Name Is : " + fullName);
+
+console.log(fullName + " is my name");
+
+console.log("My Name Is "+fname+"Mamun");
+
+var num1 = 20;
+var num2 = 30;
+
+console.log("Num1 = "+num1 +" and Num2 = "+num2);
+
+//concate by "concate function"
+var text6 = "Bangladesh";
+var text7 = " is a beautiful country";
+var text8 = text6.concat(text7);
+document.write(text8);
