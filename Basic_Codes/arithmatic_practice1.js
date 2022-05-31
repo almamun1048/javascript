ab1939286ec042bb08a7d4264220ce3efcb3819b
almamun1048 <programmeralmamun@gmail.com>
2022-05-31 23:37:29 +0600

    simple calculator

---
+++ Basic_Codes/arithmatic_practice1.js
@@ -0,0 +1,20 @@
+
+
+var num1,num2,result;
+
+num1 = parseInt(prompt("Enter first Number : "));
+num2 = parseInt(prompt("Enter second Number : "));
+
+document.write("First Number = "+ num1 +"<br/> Second Number = "+ num2 +"<br/>");
+
+result = num1+num2;
+document.write(num1 +" + "+ num2 +"= "+result +"<br/>");
+
+result = num1-num2;
+document.write(num1 +" - "+ num2 +" = "+result +"<br/>");
+
+result = num1*num2;
+document.write(num1 +" * "+ num2 +" = "+result +"<br/>");
+
+result = num1/num2;
+document.write(num1 +" / "+ num2 +" = "+result +"<br/>");
