66fb89ac3d626ae23eb046a58903ed12cd157d25
almamun1048 <programmeralmamun@gmail.com>
2022-05-31 23:42:19 +0600

    relational_operator

---
+++ Basic_Codes/relational_operator.js
@@ -0,0 +1,36 @@
+// Relational Operator =>   >,>=,<,<=,==,===,!=,!==
+
+//Logical Operator => &&,||,!
+
+var num1 = 10,num2 = 5,num3 = 25,num4 = 15;
+
+
+//check num1 greater than num2
+console.log(num1>num2);
+
+//check num1 greater than or equal num2
+console.log(num1>=num2);
+
+//check num1 less than num2
+console.log(num1<num2);
+
+//check num1 less than or equal num2
+console.log(num1<=num2);
+
+//check num1 equal to num2 without datatype
+console.log(num1==num2);
+
+//check num1 equal to num2 with datatype
+console.log(num1===num2);
+
+//check num1 not equal to num2 without datatype
+console.log(num1!=num2);
+
+//check num1 not equal to num2 with datatype
+console.log(num1!==num2);
+
+//&& if all condition become true = true
+console.log(num1>num2 && num3>num4);
+
+//|| if one condition become true = true
+console.log(num1>num2 || num3>num4);
