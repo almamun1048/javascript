1150170f186de8b8fed1fbe20062356ac3e6adc9
almamun1048 <programmeralmamun@gmail.com>
2022-05-31 23:48:08 +0600

    for loop

---
+++ Basic_Codes/loop_for.js
@@ -0,0 +1,14 @@
+
+// for loop
+
+//print text 10 times
+for(var i = 0; i<10 ; i++){
+    document.write("I Love Bangladesh <br/> <br/>");
+}
+
+//print summetion 1 to 5
+var sum = 0;
+for(var i = 1; i<6 ; i++){
+     sum = sum + i;
+}
+document.write("Sum 1 to 5 = "+sum);
