4ad7d0f20a976bdb97d350d918006001065f4875
almamun1048 <programmeralmamun@gmail.com>
2022-05-31 23:39:10 +0600

    area of rectangle

---
+++ Basic_Codes/arithmatic_practice2.js
@@ -0,0 +1,11 @@
+var base = parseInt(prompt("Enter base : "));
+var height = parseInt(prompt("Enter height : "));
+
+document.write("Base = "+base +"<br/>");
+document.write("Height = "+height +"<br/><br/>");
+
+var area = base * height ;
+document.write("Rectangle Area = "+area +"<br/>");
+
+var area = 0.5 * base * height ;
+document.write("Triangle Area = "+area +"<br/>");
