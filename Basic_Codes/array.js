c0c26ddfc71e777344b3d50c9ac316f6dfb35cf0
almamun1048 <programmeralmamun@gmail.com>
2022-06-01 01:32:44 +0600

    create array

---
+++ Basic_Codes/array.js
@@ -0,0 +1,20 @@
+//Array is an object; collection of variable
+
+//var names = new Array(4);
+var names = new Array();
+
+names[0] = "Al Mamun";
+names[1] = "Sabbir";
+names[2] = "Robi";
+names[3] = "Robin";
+
+console.log(names);
+console.log(names.length);
+console.log(names[2]);
+
+//second way to create a array
+var namesTwo = ["Nipa","Mostofa","Mamun","Kamrul","Nishi"];
+console.log(namesTwo);
+console.log(namesTwo.length);
+console.log(namesTwo[2]);
+
