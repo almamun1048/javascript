2fcc8e69f4150a40c579a04a090461ae08894460
almamun1048 <programmeralmamun@gmail.com>
2022-06-01 00:00:25 +0600

    traditional function

---
+++ Basic_Codes/function_traditional.js
@@ -0,0 +1,28 @@
+//Traditional Function
+
+
+//creating a function for square
+function square(num1){
+    var result = num1 * num1;
+    return result;
+}
+//creating a function for add
+function add(num1,num2){
+    var result = num1 + num2;
+    return result;
+}
+//creating a function for square
+function sub(num1,num2){
+    var result = num1 - num2;
+    return result;
+}
+
+//calling function
+var x = square(5);
+document.write(x+"<br/>");
+
+var x = add(5,6);
+document.write(x+"<br/>");
+
+var x = sub(15,5);
+document.write(x+"<br/>");
