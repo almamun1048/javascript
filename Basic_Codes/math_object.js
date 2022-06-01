a24a6f2e6be38bdc0855a9516d19de67cf68c7b0
almamun1048 <programmeralmamun@gmail.com>
2022-06-01 22:16:14 +0600

    math_object with library function

---
+++ Basic_Codes/math_object.js
@@ -0,0 +1,33 @@
+//Math object
+
+Math.sqrt(9);
+//result 3
+Math.abs(-6)
+//result 6
+Math.sin(2)
+//result 0.9092974268256817
+Math.pow(2,3)
+//result 8
+Math.floor(2.4)
+//result 2
+Math.ceil(2.4)
+//result 3
+Math.round(2.4)
+//result 2
+Math.round(2.5)
+//result 3
+Math.max(2,3,4,65,1,5,6)
+//result 65
+
+//0 to 1 random number genarate not 1
+Math.random();
+0.6304719636164249
+Math.random()*6;
+4.6218482546975
+
+//1 to 5 random  number genarate
+Math.floor(Math.random()*5)+1;
+5
+//10 to 20 random number genarate
+Math.floor(Math.random()*11)+10;
+13
