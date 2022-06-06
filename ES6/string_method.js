ddfd2140a8b4513e60c4808feca09c6cf79a97f4
almamun1048 <programmeralmamun@gmail.com>
2022-06-06 23:04:36 +0600

    string_method startsWith endsWith includes

---
+++ ES6/string_method.js
@@ -0,0 +1,9 @@
+//String method-> startsWith,endsWith,includes
+
+const message = "Today is thursday";
+
+console.log(message.startsWith('Today'))//start 0 index
+console.log(message.startsWith('Today',1))//start 1 index
+
+console.log(message.endsWith('thursday'))
+console.log(message.includes('is'))
